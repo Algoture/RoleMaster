2a1e2edb45971baa6c5835509400fa054926c29f
Umesh Nagare <umeshnagare27@gmail.com>
2024-12-19 13:01:36 +0530

    search bar changes

--- src/app/Components/SearchBar.jsx
+++ src/app/Components/SearchBar.jsx
@@ -2,9 +2,9 @@
 
 import { SearchIcon } from "./Icons";
 
-export const SearchBar = ({ handleSearch, search = "",placeholder }) => {
+export const SearchBar = ({ handleSearch, search = "", placeholder }) => {
   return (
-    <div className="items-center shadow-sh md:ml-0 sm:ml-12 mx-auto flex h-10 overflow-hidden w-full justify-center bg-gray-200 rounded-lg gap-2 py-2 px-4">
+    <div className="items-center shadow-sh md:ml-0 sm:ml-12 mx-auto flex h-10 overflow-hidden w-full justify-center bg-gray-200 rounded-lg  p-2">
       <SearchIcon />
       <input
         type="text"
@@ -12,7 +12,7 @@ export const SearchBar = ({ handleSearch, search = "",placeholder }) => {
         placeholder={placeholder}
         onChange={handleSearch}
         defaultValue={search || ""}
-        className="outline-none bg-transparent placeholder-slate-500 text-gray-600 text-left"
+        className="outline-none bg-transparent border-none placeholder-slate-500 text-gray-600 text-left -ml-3 "
       />
     </div>
   );
